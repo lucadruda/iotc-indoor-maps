588525769f874c5aab566063829e1de9220d01f8
lucadruda <ludruda@microsoft.com>
2021-09-15 15:24:20 +0200

    add azure function

---
+++ map_fn/IndoorMap/index.ts
@@ -0,0 +1,24 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { promises as fs } from 'fs';
+
+const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+    if (req.method === 'GET') {
+        // page requested
+        context.res = {
+            body: await fs.readFile('./public/index.html')
+        }
+    }
+    context.log('HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+
+};
+
+export default httpTrigger;
