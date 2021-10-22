baeb4f9b8a12ab9b554beaa77a974e14a70efc4b
ludruda <ludruda@microsoft.com>
2021-10-22 18:30:22 +0200

    fix output

--- setup/src/azure/deployment_az.sh
+++ setup/src/azure/deployment_az.sh
@@ -16,5 +16,3 @@ npm run build
 az storage blob service-properties update --account-name "$STORAGE_ACCOUNT_NAME" --static-website --404-document error.html --index-document index.html
 az storage blob upload-batch --account-name "$STORAGE_ACCOUNT_NAME" -d "\$web" -s build
 
-# Get web endpoint
-echo "{\"webEndpoint\":`az storage account show --name "$STORAGE_ACCOUNT_NAME" | jq .primaryEndpoints.web`}" > $AZ_SCRIPTS_OUTPUT_PATH
